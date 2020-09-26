c7b7f72776223de7494bb83df4ded936688df5d5
rikire <070998nmis@mail.ru>
2020-09-26 11:14:07 +0500

    26.09.2020

---
+++ Cickl/c/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N, a=1;
+    cin >> N;
+    for (int i=1; i<=N; i++){
+    a = a*2;
+    }
+    cout << a << endl;
+    return 0;
+}
